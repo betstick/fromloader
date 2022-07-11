0b10336ce8e5a2c04d5ef0a05eb11207c4a98453
betstick <61669265+betstick@users.noreply.github.com>
2022-07-11 16:10:42 -0400

    init

---
+++ src/util/stringio/stringio.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <stdio.h>
+#include <string>
+#include <stdexcept>
+#include <stdbool.h>
+
+#include "../endian/endian.h"
+#include "umem.h"
+
+#ifndef CFR_STRINGS
+#define CFR_STRINGS
+
+namespace cfr
+{
+	//reads string till length or null term and returns it
+	//throws exceptions on eof
+	std::string freadString(UMEM* src, bool nullTerm = true, int length = 0);
+
+	//returns just the file name when given a full path
+	//throws exceptions if it doesn't get what it wants
+	//std::string fileNameFromPath(std::string path);
+
+	//returns the file name and the path as a pair
+	std::pair<std::string,std::string> splitFullPath(std::string fullpath);
+
+	std::wstring readWString(UMEM* src, bool endian);
+
+	std::string readString(UMEM* src);
+
+	int getWideStringLength(wchar_t* str);
+
+	//seeks to string, reads till null, returns wchar array, sets len, returns to start
+	wchar_t* getUniversalString(long offset, bool unicode, UMEM* src, int* len);
+};
+
+#endif
